803da37e7a5b3a534072ee79172d32906cbe3e3a
Dustin Ogan <dustin.x.ogan.-nd@disney.com>
2020-04-08 08:59:43 -0400

    rename testing class and update .gitignore properties. Build in order dependency since the Pet API is open and dynamic to ensure testing criteria is met

--- src/test/java/automationSample/PetShopApiTest.java
+++ src/test/java/automationSample/PetShopApiTest.java
@@ -14,23 +14,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
-public class hello {
-    @Test
-    void getPetDetailsTest() {
-        RestAssured.baseURI = "https://petstore.swagger.io/v2/pet/";
-        final RequestSpecification httpRequest = RestAssured.given();
-        final Response response = httpRequest.request(Method.GET, "1774313072");
-        final int statusCode = response.getStatusCode();
-        assertEquals(statusCode, 200);
-
-    };
+public class PetShopApiTest {
+    private static final String POST_PET_DETAILS_TEST = "postPetDetailsTest";
+    final String NAME = "hippo";
+    final String STATUS = "available";
+    final String PHOTO_URL = "www.myphoto.com";
+    final String CATEGORY = "cuddly";
 
     @Test
     void postPetDetailsTest() {
-
-        final String NAME = "hippo";
-        final String STATUS = "available";
-        final String PHOTO_URL = "www.myphoto.com";
 
         RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
         final RequestSpecification httpRequest = RestAssured.given();
@@ -39,7 +31,7 @@ public class hello {
         /** Build out Our Pet Object using the PetShope API Specification */
         final JSONObject category = new JSONObject();
         category.put("id", 100);
-        category.put("name", "cuddly");
+        category.put("name", CATEGORY);
 
         final JSONArray tags = new JSONArray();
 
@@ -47,7 +39,7 @@ public class hello {
         photoUrls.add(PHOTO_URL);
 
         final JSONObject pet = new JSONObject();
-        pet.put("id", 0);
+        pet.put("id", 61985);
         pet.put("name", NAME);
         pet.put("status", STATUS);
         pet.put("tags", tags);
@@ -71,10 +63,23 @@ public class hello {
         final String status = petResponse.get("status");
         assertEquals(status, STATUS);
 
-        final ArrayList photos = petResponse.get("photoUrls");
+        final ArrayList<String> photos = petResponse.get("photoUrls");
         assertEquals(photos.size(), 1);
         assertEquals(photos.get(0), PHOTO_URL);
-
-        
     }
+
+    @Test(dependsOnMethods = { POST_PET_DETAILS_TEST })
+    void getPetDetailsTest() {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2/pet/";
+        final RequestSpecification httpRequest = RestAssured.given();
+        final Response response = httpRequest.request(Method.GET, "61985");
+
+        final int statusCode = response.getStatusCode();
+        assertEquals(statusCode, 200);
+
+        final JsonPath petResponse = response.jsonPath();
+        final String animalName = petResponse.get("name");
+        assertEquals(animalName, NAME);
+
+    };
 }
